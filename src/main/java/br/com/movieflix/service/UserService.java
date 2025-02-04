a93f0349103776d9e8cfc03814588d87ba964e1d
Igor Pardinho <pardinhoigor@gmail.com>
2025-02-04 12:04:53 -0300

    :sparkles: feat: cria o service de User

---
+++ src/main/java/br/com/movieflix/service/UserService.java
@@ -0,0 +1,23 @@
+package br.com.movieflix.service;
+
+import br.com.movieflix.entity.User;
+import br.com.movieflix.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
